ea89b355d2e781a5dff8767f3dc7f068dccd15fb
Naturalis007 <tamara.salasvergara@gmail.com>
2023-08-09 20:10:33 +0000

    completé post y delete según instrucciones

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
 
@@ -10,16 +10,23 @@ todos = [
 @app.route('/todos', methods=['GET'])
 def hello_world():
     json_text = jsonify(todos)
-    return json_text
+    return json_text 
 
 
 @app.route('/todos', methods=['POST'])
-def add_new_todo(todos):
-    request_body = request.json
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
     print("Incoming request with the following body", request_body)
-    todos.append(request_body)
-    return jsonify(todos)
+    return  jsonify(todos)
 
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
 
 
 
